96198b63f981b20af9b76b819d422b2d7992d29c
Baran Göcen <barangokselgocen@icloud.com>
2022-09-13 11:19:06 +0300

    Initial Commit

---
+++ Car4Future Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Car4Future Task
+//
+//  Created by Baran Göcen on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
